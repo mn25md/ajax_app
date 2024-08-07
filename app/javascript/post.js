00a1660936f82cf966f8d2335cceb44340d577c3
mn25md <mn25md@gmail.com>
2024-08-08 05:57:51 +0900

    修正

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -3,7 +3,11 @@ function post (){
   form.addEventListener("submit", (e) => {
     const formData = new FormData(document.getElementById("form"));
     const XHR = new XMLHttpRequest();
+<<<<<<< Updated upstream
     XHR.open("POST", "/posts", true);
+=======
+    XHR.open("POST", "/articles", true);
+>>>>>>> Stashed changes
     XHR.responseType = "json";
     XHR.send(formData);
     e.preventDefault();
